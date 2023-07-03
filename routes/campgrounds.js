9d181bd1a246b2607319bdaf590fcdefbf44ad07
Kashyap Chaganti <Kashyapchaganti96@gmail.com>
2023-07-02 21:13:49 -0400

    Authorization Updated

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,26 +1,32 @@
+// const express = require('express');
+// const router = express.Router();
+// const campgrounds = require('../controllers/campgrounds');
+// const catchAsync = require('../utils/catchAsync');
+// const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+// const {storage}= require('../cloudinary');
+// const multer  = require('multer')
+// const upload = multer({ storage})
 const express = require('express');
 const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
-const {storage}= require('../cloudinary');
-const multer  = require('multer')
-const upload = multer({ storage})
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-    .post(upload.array('image'), (req,res)=>{
-        console.log(req.body, req.file)
-    })
+    .post(isLoggedIn, upload.array('image'),validateCampground, catchAsync(campgrounds.createCampground))
+    
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .put(isLoggedIn, isAuthor, upload.array('image'),validateCampground, catchAsync(campgrounds.updateCampground))
     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
--- seeds/index.js
+++ seeds/index.js
@@ -33,16 +33,27 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 // }
 const seedDB= async () =>{
     await Campground.deleteMany({});
-    for(let i=0;i<50;i++){
+    for(let i=0;i<300;i++){
         const random1000 =Math.floor(Math.random()*1000);
         const price =Math.floor(Math.random()*20)+10;
         const camp= new Campground({
             author:'6496859af6cd9dbe67435332',
             title: `${sample(descriptors)} ${sample(places)}`,
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            image:'https://source.unsplash.com/collection/483251',
+            images: [
+                {
+                  url: 'https://res.cloudinary.com/dtlukvxux/image/upload/v1687909397/YelpCamp/e4gkfx5ul07vxbosnih7.jpg',
+                  filename: 'YelpCamp/e4gkfx5ul07vxbosnih7',
+                  
+                }
+              ],
             price,
-            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maiores minus molestias placeat ea vero non? Quam odio veritatis harum corrupti dignissimos in eligendi aut et? Optio facilis distinctio id. '
+            geometry:{
+              type:"Point",
+              coordinates:[cities[random1000].longitude,
+              cities[random1000].latitude]
+            }
+            ,description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maiores minus molestias placeat ea vero non? Quam odio veritatis harum corrupti dignissimos in eligendi aut et? Optio facilis distinctio id. '
         })
         await camp.save();
     }
